7a871b30506608223d5eac9fba1ba296b900c541
Raul Dias <raul@dias.com.br>
2024-10-07 12:42:01 -0300

    removed UMD support and Babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,17 @@
 const path = require('path');
 
-const pascalToKebab = (str) =>
-    str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+const pascalToKebab = (str) => {
+    str = str.replace(/PhotoSwipe/g, 'Photoswipe');
+    return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
 
-const commonConfig = {
+module.exports = {
     entry: {
         PhotoSwipeVideoServicePlugin: './src/main-video-service.js',
         PhotoSwipeYoutubePlugin: './src/main-youtube.js',
         PhotoSwipeVimeoPlugin: './src/main-vimeo.js',
     },
     mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        // Ensure Babel preserves ES modules for ESM build
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: false, // Important for ESM
-                                },
-                            ],
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-    },
-    externals: {
-        // Exclude dependencies that the library user will provide
-        photoswipe: 'photoswipe',
-        'photoswipe/lightbox': 'photoswipe/lightbox',
-    },
-};
-
-const umdConfig = {
-    ...commonConfig,
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        library: '[name]',
-        libraryTarget: 'umd',
-        globalObject: 'this',
-    },
-};
-
-const esmConfig = {
-    ...commonConfig,
     output: {
         filename: (pathData) => {
             const name = pathData.chunk.name;
@@ -73,26 +26,24 @@ const esmConfig = {
     experiments: {
         outputModule: true,
     },
-    // Ensure Babel does not transform ES modules
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: false, // Preserve ES modules
-                                },
-                            ],
-                        ],
-                    },
-                },
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    externals: {
+        // Exclude dependencies that the library user will provide
+        photoswipe: 'photoswipe',
+        'photoswipe/lightbox': 'photoswipe/lightbox',
+    },
+    module: {
+        rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
@@ -100,5 +51,3 @@ const esmConfig = {
         ],
     },
 };
-
-module.exports = [umdConfig, esmConfig];
